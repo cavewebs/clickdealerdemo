97222070ea32288dc008a5dc0de58a19893128cd
Tertsea Uzua <tim@192.168.1.116>
2023-01-14 11:31:26 +0000

    Initial commit

---
+++ test/clickdealer-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ClickdealerDemo from '../lib/clickdealer-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/clickdealer-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ClickdealerDemo.ClickdealerDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
